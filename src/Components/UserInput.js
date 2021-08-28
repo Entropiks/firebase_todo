dccb30c0ff9300621eb1f3c1721fb282f6dc3b46
Entropiks <mikecorseri@gmail.com>
2021-08-28 13:41:51 -0700

    working on props to pass user submits

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UserInput.css';
 
 function UserInput() {
+
+  // new state variable for when a user inputs something
+  const [item, setUserItem] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`item: ${item}`);
+  }
+
   return (
     <div className="input-wrapper">
       <div className="input-toggle">
         <div className="checkmark"></div>
       </div>
       <div className="input-field">
-        <form>
-          <input type="text" placeholder="Enter an item.." autofocus></input>
+        <form onSubmit={handleSubmit}>
+          <input 
+            value={item}
+            type="text" 
+            placeholder="Enter an item.."
+            onChange={e => setUserItem(e.target.value)}
+            />
+          <input type="submit" value="Submit"/>
         </form>
       </div>
     </div>
@@ -17,3 +32,5 @@ function UserInput() {
 }
 
 export default UserInput
+
+
