60ecbb077b245907ee576c5a85a352dd0bed72ad
entropiks <mikecorseri@gmail.com>
2021-09-15 21:49:38 -0700

    just moving through

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -2,14 +2,12 @@ import React from 'react'
 import UserItem from './UserItem'
 import './UserList.css'
 
-function UserList({items}) {
-  console.log(items);
+function UserList() {
+
   return (
     <div className="user-list">
       <ul className="list-wrapper">
-        {items.map((item) => (
-          <UserItem text={item.text}/>
-        ))}
+       
       </ul>
     </div>
   )
