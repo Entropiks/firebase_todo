539ea066ee5395aa8954dcfbfdad1efffac95fcf
entropiks <mikecorseri@gmail.com>
2021-10-31 21:34:22 -0700

    tidy up

--- src/Components/ThemeToggle.js
+++ src/Components/ThemeToggle.js
@@ -5,6 +5,7 @@ function ThemeToggle() {
   return (
     <div>
       <div className="button r" id="button-1">
+        {/* When the input is clicked here, we need to change styles */}
         <input type="checkbox" className="checkbox" />
         <div className="knobs"></div>
         <div className="layer"></div>
